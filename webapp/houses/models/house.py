7af3c3a08e835745224ff4680218d81c493c74e7
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-31 18:40:19 +0900

    feat(house): house 모델 수정

--- webapp/houses/models/house.py
+++ webapp/houses/models/house.py
@@ -10,6 +10,7 @@ class House(models.Model):
         verbose_name_plural = 'Houses'
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
