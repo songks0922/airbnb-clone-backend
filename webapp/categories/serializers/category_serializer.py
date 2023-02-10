a0b6bd1ca6db1625506aeacf3b65503dd7abaa3f
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-02-11 02:31:38 +0900

    refactor: Serializer -> ModelSerializer으로 리팩토링

--- webapp/categories/serializers/category_serializer.py
+++ webapp/categories/serializers/category_serializer.py
@@ -2,26 +2,7 @@ from rest_framework import serializers
 from categories.models import Category
 
 
-class CategorySerializer(serializers.Serializer):
-    pk = serializers.IntegerField(
-        read_only=True
-    )
-    name = serializers.CharField(
-        required=True,
-        max_length=50,
-    )
-    kind = serializers.ChoiceField(
-        choices=Category.CategoryKindChoices.choices,
-    )
-    created_at = serializers.DateTimeField(
-        read_only=True
-    )
-
-    def create(self, validated_data):
-        return Category.objects.create(**validated_data)
-
-    def update(self, category, validated_data):
-        category.name = validated_data.get('name', category.name)
-        category.kind = validated_data.get('kind', category.kind)
-        category.save()
-        return category
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        exclude = ['created_at', 'updated_at']
